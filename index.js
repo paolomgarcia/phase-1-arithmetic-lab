4f631c2489c978c00346cc27848486f859d939e4
Paolo Garcia <pmatteo1108@yahoo.com>
2022-09-20 16:00:36 -0600

    commit tree 2ffdf15bedb3aeb6d7d8893674089cc46c64d69f

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1,4,6,20,18);
+
